a110e348a5772252f615d0182264d5d10b2c265d
gema_handika <gemahandika88@gmail.com>
2025-02-13 11:01:34 +0700

    update

--- app/config/koneksi.php
+++ app/config/koneksi.php
@@ -1,9 +1,9 @@
 <?php
 // koneksi local
-$koneksi = mysqli_connect('localhost', 'root', '', 'db_berita');
-if (mysqli_connect_errno()) {
-    echo mysqli_connect_error();
-}
+// $koneksi = mysqli_connect('localhost', 'root', '', 'db_berita');
+// if (mysqli_connect_errno()) {
+//     echo mysqli_connect_error();
+// }
 
 // koneksi public
 $koneksi = mysqli_connect('localhost', 'supr3895_root', ',YfZ_RpZ#k1m', 'supr3895_db_berita');
