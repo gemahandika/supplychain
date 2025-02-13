7273467c210d88ab0889294a92c8b6fd8efb920b
gema_handika <gemahandika88@gmail.com>
2025-02-13 16:52:34 +0700

    Update

--- public/footer.php
+++ public/footer.php
@@ -3,16 +3,6 @@
     <div class="footer-newsletter">
         <div class="container">
             <div class="row justify-content-center text-center">
-                <div class="col-lg-6">
-                    <h4>Join Our Newsletter</h4>
-                    <p>Subscribe to our newsletter and receive the latest news about our products and services!</p>
-                    <form action="forms/newsletter.php" method="post" class="php-email-form">
-                        <div class="newsletter-form"><input type="email" name="email"><input type="submit" value="Subscribe"></div>
-                        <div class="loading">Loading</div>
-                        <div class="error-message"></div>
-                        <div class="sent-message">Your subscription request has been sent. Thank you!</div>
-                    </form>
-                </div>
             </div>
         </div>
     </div>
@@ -21,7 +11,7 @@
         <div class="row gy-4">
             <div class="col-lg-4 col-md-6 footer-about">
                 <a href="index.html" class="d-flex align-items-center">
-                    <span class="sitename">FlexStart</span>
+                    <span class="sitename">Supply Chain</span>
                 </a>
                 <div class="footer-contact pt-3">
                     <p>A108 Adam Street</p>
